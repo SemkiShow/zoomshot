0e8ddb8dc812d37aaed74d5e4f0f8d02a1ef9713
SemkiShow <mgdeveloper123@gmail.com>
2026-06-04 23:06:07 +0200

    Add raylib

--- main.c
+++ main.c
@@ -1,7 +1,29 @@
+#include <raylib.h>
 #include <stdio.h>
 
 int main(void)
 {
-    printf("Hello, World!\n");
+    unsigned int flags = 0;
+    flags |= FLAG_VSYNC_HINT;
+    flags |= FLAG_FULLSCREEN_MODE;
+    flags |= FLAG_WINDOW_UNDECORATED;
+    flags |= FLAG_WINDOW_TOPMOST;
+    SetConfigFlags(flags);
+
+    InitWindow(800, 600, "zoomshot");
+
+    while (!WindowShouldClose())
+    {
+        BeginDrawing();
+
+        ClearBackground(BLACK);
+
+        DrawRectangle(0, 0, 100, 100, RED);
+
+        EndDrawing();
+    }
+
+    CloseWindow();
+
     return 0;
 }
--- nob.c
+++ nob.c
@@ -2,6 +2,12 @@
 #define NOB_EXPERIMENTAL_DELETE_OLD
 #include "nob.h"
 
+void raylib(Nob_Cmd* cmd)
+{
+    nob_cmd_append(cmd, "-I.", "-I./raylib-6.0_linux_amd64/include");
+    nob_cmd_append(cmd, "-L./raylib-6.0_linux_amd64/lib", "-l:libraylib.a", "-lm", "-lX11");
+}
+
 int main(int argc, char** argv)
 {
     NOB_GO_REBUILD_URSELF(argc, argv);
@@ -9,6 +15,7 @@ int main(int argc, char** argv)
     Nob_Cmd cmd = {0};
     nob_cmd_append(&cmd, "cc", "-Wall", "-Wextra", "-o", "zoomshot");
     nob_cmd_append(&cmd, "main.c");
+    raylib(&cmd);
     if (!nob_cmd_run(&cmd)) return 1;
 
     return 0;
